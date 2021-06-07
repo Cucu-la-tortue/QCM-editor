9b08423583a7c8ca02e8d686ca2697f64d7704ce
Titi-la-brebis <timothee.fevrier@icloud.com>
2021-06-08 00:41:28 +0200

    Add home-page

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QCM Maker</title>
+    <!-- Css -->
+    <!-- index -->
+    <link rel="stylesheet" href="./css/index.css">
+    <!-- header -->
+    <link rel="stylesheet" href="./css/header.css">
+</head>
+<body>
+  <header>
+    <div class="qcm-header__logo">
+        <img src="./assets/logo.svg" alt="logo" />
+        <h3>QCM Maker</h3>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="login.php">Sign in</a></li>
+            <li><a href="register.php" class="btn btn--header">Sign up</a></li>
+        </ul>
+    </nav>
+  </header>
+
+  <section class= 'main-page'>
+
+    <div class='qcm__presentation'>
+      <div class='title'>
+        <img src="./assets/logo.svg" alt="logo">
+        <h1>Votre QCM en ligne</h1>
+      </div>
+      <p>Vous voici dans votre site de QCM en ligne. Ici, résolvez vos QCM personnalisés et faites le tester à vos amis !</p>
+    </div>
+    <div class='qcm-action-box'>
+      <div class="qcm-actions__title">
+          <h2>Log out</h2>
+          <img src="./assets/exit-icon.svg" alt="icon">
+      </div>
+      <p class="qcm-actions__details">Session over? Don't forget to log out! See you soon on QCM Maker.</p>
+      <a href="#" class="btn btn--actions">Tschuss</a>
+    </div>
+
+  </section>
+
+    
+  </body>
+</html>
